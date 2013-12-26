60ee00e82e565b2c750bdc008bea4ff46d916348
mathewxiang <xiangzhuyuan@gmail.com>
2013-12-27 01:20:46 +0800

    Initial Commit

---
+++ xiangzhuyuan_objectiveC_1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xiangzhuyuan_objectiveC_1
+//
+//  Created by xiangzhuyuan on 13-12-27.
+//  Copyright (c) 2013年 xiangzhuyuan.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
